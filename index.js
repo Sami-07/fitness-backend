59e0afd35bd1edfcf58b834d097abb729388d271
sami <s.a.sami359359@gmail.com>
2024-01-01 14:15:43 +0530

    cors fixes 2

--- index.js
+++ index.js
@@ -4,51 +4,34 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dashboardRoutes from "./routes/dashboard.js";
 import 'dotenv/config'
-// import { auth } from "./config/firebaseConfig.js"
 import User from "./models/User.js";
-// import  admin from "firebase-admin"
-// import serviceAccount from "./config/serviceAccount.json" assert {type: "json"}
-import admin from "firebase-admin"
-const app = express();
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 
+const app = express();
 app.use(cors({
     origin: "https://fitness-freak-xi.vercel.app",
     methods: ["POST", "GET"],
     credentials: true
 }));
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount)
-// })
+// app.use(bodyParser.json({ limit: "30mb", extended: true }))
+// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+// });
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
     .catch(err => console.log(err.message));
 
-// app.use(async (req, res, next) => {
-//     const idToken = req.headers.authorization.split(' ')[1];
-//     console.log("id in use", idToken);
-//     try {
-//         const decodedToken = await admin.auth.verifyIdToken(idToken);
-//         req.user = decodedToken; // Attach the user information to the request object
-//         console.log("req", req.user)
-//         next();
-//     } catch (error) {
-//         // Handle authentication failure
-//         res.status(401).json({ error: 'Unauthorized' });
-//     }
-// });
+
 
 app.post("/register", async function (req, res) {
-    const { userName, email } = req.body;
+
 
 
     let user = new User({
@@ -68,7 +51,6 @@ app.post("/registergoogleuser", async function (req, res) {
         res.json({ status: true });
     }
     else {
-
         let user = new User({
             name: req.body.displayName,
             email: req.body.email
@@ -77,18 +59,7 @@ app.post("/registergoogleuser", async function (req, res) {
         res.json({ status: true })
     }
 
-
-
 })
-
-
-
-
-
-
-
-
-
 
 //here, all the routes inside dashboardRoutes starts from "https://fitness-webapp-backend.vercel.app/dashboard"
 app.use("/dashboard", dashboardRoutes);
